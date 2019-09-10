29b635908587f42d89719c1daf2af497cfbbd7c7
Derick Ostrenko <dostrenko@gmail.com>
2019-09-10 14:40:40 -0500

    added files to repo

---
+++ sketch.js
@@ -0,0 +1,37 @@
+function setup() {
+  createCanvas(400, 269);
+  background(150);
+
+  beginShape();
+  vertex(10, 10);
+  vertex(50, 10);
+  vertex(50, 50);
+  vertex(25, 100);
+  vertex(30, 70);
+  endShape(CLOSE);
+
+  // strokeWeight(50);
+  // point(width / 2, height / 2);
+  // strokeWeight(1);
+  // ellipse(10, 10, 10);
+  // strokeWeight(10);
+  // line(100, 0, width - 10, height - 100);
+  // line(0, 0, width - 100, height);
+  // noStroke();
+  // fill(0, 255, 0, 255);
+  // rect(50, 50, 50, 50);
+  // // fill("red");
+  // stroke("orange");
+  // noFill();
+  // arc(25, 25, 100, 100, radians(0), radians(90));
+
+  // stroke("purple");
+
+  // point()
+
+  // curve(-1000, -1000, 0, height / 2, width, height / 2, width - 1000, height - 1000);
+}
+
+function draw() {
+
+}
